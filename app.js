f94715b5ee7891a5d261d25b05dfcb31e525d173
mstrangtrieueducation-droid <mstrangtrieueducation@gmail.com>
2026-08-05 18:03:24 +0700

    Publish Discover 1 Unit 14 Vocabulary

--- app.js
+++ app.js
@@ -53,7 +53,7 @@
   }
 
   function imagePath(item) {
-    return `assets/images/${item.slug}.webp?v=20260805`;
+    return `assets/images/${item.slug}.webp?v=20260805-hires-u14`;
   }
 
   function audioPath(item) {
